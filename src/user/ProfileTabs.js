51fda1931e5d44247f7c1b49ceac3850baf2b9f7
Anirban Banerjee <abanerjee763@outlook.com>
2021-08-18 09:41:51 +0530

    Final

--- src/user/ProfileTabs.js
+++ src/user/ProfileTabs.js
@@ -4,48 +4,12 @@ import DefaultProfile from "../images/images.png"
 
 class ProfileTabs extends Component {
     render() {
-        const { following, followers, posts } = this.props;
+        const { posts } = this.props;
         return (
             <div>
             <div className="row">
-      
-                <div className="col-md-12">
-                    <h3 className="text-primary">
-                        {following.length} Following
-                    </h3>
-                    <hr />
-                    {following.map((person, i) => (
-                        <div key={i}>
-                            <div>
-                                <Link to={`/user/${person._id}`}>
-                                    <img
-                                        style={{
-                                            borderRadius: "50%",
-                                            border: "1px solid black"
-                                        }}
-                                        className="float-left mr-2"
-                                        height="30px"
-                                        width="30px"
-                                        onError={i =>
-                                            (i.target.src = `${DefaultProfile}`)
-                                        }
-                                        src={`${
-                                            process.env.REACT_APP_API_URL
-                                        }/user/photo/${person._id}`}
-                                        alt={person.name}
-                                    />
-                                    <div>
-                                        <p className="lead">
-                                            {person.name}
-                                        </p>
-                                    </div>
-                                </Link>
-                            </div>
-                        </div>
-                    ))}
-                </div>
 
-                <div className="col-md-4">
+                <div className="col-md-12">
                     <h3 className="text-primary">{posts.length} Posts</h3>
                     <hr />
                     {posts.map((post, i) => (
