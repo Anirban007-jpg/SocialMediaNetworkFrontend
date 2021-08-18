2eb2b7ad2b2e41820b0f5f1ad5aa4e26d1455db3
Anirban Banerjee <abanerjee763@outlook.com>
2021-08-18 10:45:21 +0530

    Final

--- src/user/FollowProfileButton.js
+++ src/user/FollowProfileButton.js
@@ -5,25 +5,21 @@ import { follow, unfollow } from './apiUser';
 class FollowProfileButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            isFollowButton: false
-         }
+        this.state = {  }
     }
 
     followClick = () => {
         this.props.onButtonClick(follow);
-        this.setState({ isFollowButton: true});
     };
 
     unfollowClick = () => {
         this.props.onButtonClick(unfollow);
-        this.setState({ isFollowButton: false});
     };
     
     render() { 
         return (
             <div className="d-inline-block mt-5">
-                 {!this.props.following && isFollowButton ? (
+                 {!this.props.following ? (
                     <button
                         onClick={this.followClick}
                         className="btn btn-success btn-raised mr-5"
